403d3a70275767c86d44be9a81281794e7851e2b
Ahmed Kacem Jebari <Ahmedkacem.jebari@esprit.tn>
2025-02-11 10:16:44 +0100

    Init

---
+++ src/test/java/tn/esprit/messagemicroservice/MessageMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.messagemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMicroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
